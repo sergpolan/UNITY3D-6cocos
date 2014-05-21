5057f1508c3c14d393d5723724ed8353676ec600
Sergio González Polán <sergpolan@gmail.com>
2014-05-21 22:48:33 +0200

    GUI

---
+++ dev/test1/Assets/Scripts/Interfaz.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+
+public var textura : Texture;
+public var estetica : GUISkin;
+
+private var texto : String;
+
+function Start()
+{
+	texto = "Pulsa";
+}
+
+
+function OnGUI()
+{
+	GUI.skin = estetica;
+	GUI.Label(Rect(10,10,200,100), texto);
+	
+	if(GUI.Button(Rect(600,10,200,100), textura))
+	{
+		texto = "OK!";
+	}
+}
