b1388ffb283e98efe92bfac170d2b91ac552c873
Sergio González Polán <sergpolan@gmail.com>
2014-10-27 11:16:56 +0100

    Create a zombie spawn

---
+++ dev/Assets/Scripts/Zombie Spawner.js
@@ -0,0 +1,29 @@
+﻿#pragma strict
+
+var zombie: GameObject;
+var spawnPosition;
+var startTime : float;
+
+function Start () 
+{
+	startTime = Time.time;
+}
+
+function Update () 
+{
+	var guiTime = Time.time - startTime;
+	var seconds : int = guiTime % 60;
+	
+	if(seconds > 5)
+	{
+		spawn();
+		startTime = Time.time;
+	}
+}
+
+function spawn ()
+{
+	spawnPosition = Vector3(0,0,0);
+	var createZombie = Instantiate(zombie, spawnPosition, Quaternion.identity);
+	Debug.Log("Se creo un zombie"); 
+}
