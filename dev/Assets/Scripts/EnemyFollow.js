82ce2402854767e36601f4c35a8804eeaccfdb0a
Sergio González Polán <sergpolan@gmail.com>
2014-10-27 11:03:56 +0100

    Fixed zombie animator controller

--- dev/Assets/Scripts/EnemyFollow.js
+++ dev/Assets/Scripts/EnemyFollow.js
@@ -1,6 +1,9 @@
-﻿var target : Transform; 
-var myTransform : Transform; 
+﻿private var target : Transform; 
+private var myTransform : Transform; 
 private var startTime;
+private var seconds : int;
+private var fraction : int;
+private var animator: Animator;
 
 var moveSpeed = 3; 
 var rotationSpeed = 3;  
@@ -14,10 +17,29 @@ function Awake()
 function Start()
 {
      target = GameObject.FindWithTag("Player").transform;
- 	
+     animator = GetComponent(Animator);
 }
  
 function Update () {
+
+ 	var guiTime = Time.time - startTime;
+    seconds = guiTime % 60;
+    fraction = (guiTime * 100) % 100;
+    
+    if(seconds < 1)
+    {
+    	animator.SetFloat("Speed", 2.0);
+    }
+    else if(seconds > 1 && seconds < 3)
+    {
+    	animator.SetBool("Run", true);
+    }
+    
+    if(seconds != 0 && seconds % 10 == 0 && fraction < 2)
+    {
+    	moveSpeed = moveSpeed + 1;
+    	Debug.Log("MoveSpeed:  " + moveSpeed);
+    }
     
     myTransform.rotation = Quaternion.Slerp(myTransform.rotation,
     Quaternion.LookRotation(target.position - myTransform.position), rotationSpeed*Time.deltaTime);
