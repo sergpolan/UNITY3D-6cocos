e9a6548bdeb9105f76f8c90d0cfa9e8bb55daa43
Sergio González Polán <sergpolan@gmail.com>
2014-10-27 10:04:08 +0100

    Add Zombie Project

---
+++ dev/Assets/Scripts/EnemyFollow.js
@@ -0,0 +1,27 @@
+﻿var target : Transform; 
+var moveSpeed = 3; 
+var rotationSpeed = 3; 
+ 
+var myTransform : Transform; 
+ 
+function Awake()
+{
+    myTransform = transform; 
+}
+ 
+function Start()
+{
+     target = GameObject.FindWithTag("Player").transform;
+ 
+}
+ 
+function Update () {
+    
+    myTransform.rotation = Quaternion.Slerp(myTransform.rotation,
+    Quaternion.LookRotation(target.position - myTransform.position), rotationSpeed*Time.deltaTime);
+ 
+    
+    myTransform.position += myTransform.forward * moveSpeed * Time.deltaTime;
+ 
+ 
+}
---
+++ dev/Assets/Scripts/buttonAction.js
@@ -0,0 +1,5 @@
+﻿#pragma strict
+
+function OnMouseDown () { // funcion que detecta cuando das click
+	Application.LoadLevel("fase01"); // linea que cargar tu escena
+}
